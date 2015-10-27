c5ba0f5385a1222db16dd413d89caaa7b2d38c19
nklmish <coding.aurora@gmail.com>
2015-10-27 21:45:43 +0100

    change visibility to public

--- comment-service/src/main/java/com/nklmish/cs/comments/Application.java
+++ comment-service/src/main/java/com/nklmish/cs/comments/Application.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-class Application {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
